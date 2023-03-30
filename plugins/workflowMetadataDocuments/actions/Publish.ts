9750ab9dfac765db8df082286c64551711750133
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2023-03-29 20:21:59 -0400

    change per hooks recommended linting

--- plugins/workflowMetadataDocuments/actions/Publish.ts
+++ plugins/workflowMetadataDocuments/actions/Publish.ts
@@ -1,29 +1,25 @@
-import {
-  DocumentActionComponent,
-  DocumentActionProps,
-  useCurrentUser,
-  userHasRole,
-} from 'sanity'
-import { useWorkflowMetadata } from '../utils/useWorkflowMetadata'
+import {DocumentActionComponent, DocumentActionProps, useCurrentUser, userHasRole, useValidationStatus} from 'sanity'
+import {useWorkflowMetadata} from '../utils/useWorkflowMetadata'
 
-export const Publish = (publishAction: DocumentActionComponent) => {
-  return (props: DocumentActionProps) => {
-    const { id, draft } = props
-    const { data } = useWorkflowMetadata(id)
+export function Publish(publishAction: DocumentActionComponent) {
+  const PublishActionWithWorkflow = (props: DocumentActionProps) => {
+    const {id, draft, type} = props
+    const {data} = useWorkflowMetadata(id)
+    const { validation, isValidating } = useValidationStatus(id, type)
 
     const user = useCurrentUser()
-    const isAdmin =
-      userHasRole(user, 'administrator') || userHasRole(user, 'editor')
+    const isAdmin = user && (userHasRole(user, 'administrator') || userHasRole(user, 'editor'))
     const originalResult = publishAction(props)
 
-    //only show if we're ready for release
+    // only show if we're ready for release
     if (data?.state !== 'readyForRelease') {
       return null
     }
 
     return {
       ...originalResult,
-      disabled: !draft || originalResult.disabled || !isAdmin,
+      disabled: !draft || originalResult?.disabled || !isAdmin || isValidating || validation.length,
     }
   }
+  return PublishActionWithWorkflow
 }
--- plugins/workflowMetadataDocuments/actions/Schedule.ts
+++ plugins/workflowMetadataDocuments/actions/Schedule.ts
@@ -1,54 +1,49 @@
-import { useEffect } from 'react'
+import {useEffect} from 'react'
 import {
-  DateInput,
   DocumentActionComponent,
   DocumentActionProps,
   useClient,
   useCurrentUser,
   userHasRole,
 } from 'sanity'
-import { useWorkflowMetadata } from '../utils/useWorkflowMetadata'
+import {useWorkflowMetadata} from '../utils/useWorkflowMetadata'
 
-export const Schedule = (scheduleAction: DocumentActionComponent) => {
-  return (props: DocumentActionProps) => {
-    const { id, draft } = props
-    const { data, setState } = useWorkflowMetadata(id)
-    const client = useClient({ apiVersion: '2023-03-15' })
+export function Schedule(scheduleAction: DocumentActionComponent) {
+  const ScheduleWithWorkflow = (props: DocumentActionProps) => {
+    const {id, draft} = props
+    const {data, setState} = useWorkflowMetadata(id)
+    const client = useClient({apiVersion: '2023-03-15'})
 
     const user = useCurrentUser()
-    const isAdmin =
-      userHasRole(user, 'administrator') || userHasRole(user, 'editor')
-    const originalResult = scheduleAction(props)
+    const isAdmin = user && (userHasRole(user, 'administrator') || userHasRole(user, 'editor'))
 
-    //scheduled publishing sends window messages. listening to them
-    //ensures we don't get into race conditions from onHandle et al.
+    // scheduled publishing sends window messages. listening to them
+    // ensures we don't get into race conditions from onHandle et al.
     useEffect(() => {
       const scheduleMessages = ['scheduleDelete', 'scheduleCreate']
-      const handleMessageEvent = async (event: MessageEvent) => {
-        if (event.type === 'scheduleDelete') {
-          //there's no more schedule, send doc back to its previous state
-          //or to beginning of workflow if needed
-          return setState('readyForRelease')
-        }
-        const projectId = client.config().projectId
+      const handleMessageEvent = async () => {
+        const {projectId} = client.config()
         const schedule = await client
           .request({
             method: 'GET',
             uri: `/schedules/${projectId}/production`,
           })
-          .then((data) =>
-            data.schedules.find((s: any) =>
-              s.documents.find((doc) => doc.documentId === id)
+          .then((scheduleData) =>
+            scheduleData.schedules.find(
+              (s: any) =>
+                s.documents.find((doc: Record<string, any>) => doc.documentId === id) &&
+                s.state === 'scheduled'
             )
           )
         if (schedule) {
           return setState('scheduled')
         }
+        return setState('readyForRelease')
       }
       scheduleMessages.forEach((message) => {
         window.addEventListener(message, handleMessageEvent)
       })
-      //cleanup listeners
+      // cleanup listeners
       return () => {
         scheduleMessages.forEach((message) => {
           window.removeEventListener(message, handleMessageEvent)
@@ -56,15 +51,18 @@ export const Schedule = (scheduleAction: DocumentActionComponent) => {
       }
     }, [client, id, setState])
 
-    //still show this if scheduled so we can reschedule
-    if (!['readyForRelease', 'scheduled'].includes(data?.state as string)) {
+    const originalResult = scheduleAction(props)
+
+    // still show this if scheduled so we can reschedule
+    if (!['readyForRelease', 'scheduled'].includes(data?.state as string) || !scheduleAction) {
       return null
     }
 
     return {
       ...originalResult,
-      disabled: originalResult.disabled || !isAdmin || !draft,
-      label: 'Schedule and Update Metadata',
+      disabled: originalResult?.disabled || !isAdmin || !draft,
+      label: 'Schedule and Change Metadata',
     }
   }
+  return ScheduleWithWorkflow
 }
